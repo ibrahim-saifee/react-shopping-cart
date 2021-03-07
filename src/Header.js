a95cb290d470077951f665e490c198a407b2d999
Ibrahim Saifee <ibrahim.saifee@infobeans.com>
2021-03-07 10:51:24 +0530

    Added logo in Header component.

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
+import "./Header.css"
 
 class Header extends Component {
   render () {
     return (
-      <div className="">
-        <img src={logo} />
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <img src={logo} className="logo" alt="Shopping cart" />
+          </div>
+          <div className="col-8">
+            col8
+          </div>
+        </div>
       </div>
     )
   }
