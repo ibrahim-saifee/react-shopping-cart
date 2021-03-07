d52915cb1025adafd62a7ea139452cd075e27d5f
Ibrahim Saifee <ibrahim.saifee@infobeans.com>
2021-03-07 10:14:03 +0530

    app setup, added CDN for bootstrap and fontawesome, added header component.

---
+++ src/Products.js
@@ -0,0 +1,12 @@
+class Products {
+  constructor(productId, productName, description, price, quantity, imageUrl) {
+    this.productId = productId
+    this.productName = productName
+    this.description = description
+    this.price = price
+    this.quantity = quantity
+    this.imageUrl = imageUrl
+  }
+}
+
+export default Products
