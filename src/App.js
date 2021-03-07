a6a609b8345d333e3f2aa67bf793c06534c2c0b8
Ibrahim Saifee <ibrahim.saifee@infobeans.com>
2021-03-07 10:33:18 +0530

    Changed App component from functional to class to use this.state. Added click event handler.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div></div>
-  );
+class App extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      showCart: false
+    }
+  }
+
+  showButtonClicked = (isShowCart) => {
+    this.setState({showCart: isShowCart})
+  }
+
+  render () {
+    return (
+      <div>
+        <input type="button" value="Show Products" className="btn btn-success" onClick={this.showButtonClicked.bind(this, true)} />
+        <input type="button" value="Show Cart" className="btn btn-warning" onClick={this.showButtonClicked.bind(this, false)} />
+      </div>
+    );
+  }
 }
 
 export default App;
